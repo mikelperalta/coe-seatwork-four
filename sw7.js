73f5cc2da135713c6d0c9dbffa187df383909e21
mikelperalta <mikelitoperalta@gmail.com>
2014-08-09 14:48:33 +0800

    added sw7

---
+++ sw7.js
@@ -0,0 +1,20 @@
+function lettercount(s, force){
+	var cs= [], L,
+	s= ta.value.toLowerCase().split('').sort().join('');
+	var M= s.match(/([a-z])\1+/g)
+	M.sort(function(a, b){
+		return a.length> b.length? -1: 1;
+	});
+	if(force) return M;
+
+	L= M[0].length;
+	while(M.length && M[0].length== L){
+		cs.push(M.shift().charAt(0));
+	}
+	return cs.join(' and ')+' occurred '+L+' times.'
+}
+var ta={
+value: 'aaaaaabbbbbbbccccccddddddeeeeeeeffffffggggggghhhhhhhh'
+};
+
+alert(lettercount(ta.value));
